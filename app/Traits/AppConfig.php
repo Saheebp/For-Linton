0883b936b1d6ec4aeb8f69ecf41a021e21f6707c
Nnamdi Ibe <ibennamdik@gmail.com>
2022-09-22 12:54:01 +0100

    id or user when not logged in, check if guest

--- app/Traits/AppConfig.php
+++ app/Traits/AppConfig.php
@@ -8,33 +8,53 @@ use App\Models\Notification;
 use App\Models\Config;
 
 use Carbon\Carbon;
+use Auth;
 
 trait AppConfig
 {
     //check if there are new messages
     function newMessageCount()
     {
-        $count = Message::where('reply','false')->where('receiver_id',auth()->user()->id)->get()->count();
-        return $count; 
+        if (Auth::check()) {
+            $count = Message::where('reply','false')->where('receiver_id',Auth::user()->id)->get()->count();
+            return $count; 
+        }else{
+            return 0;
+        }
+        
     }
 
     function newMessages()
     {
         //$messages = Message::where('receiver_id',Auth::user()->id)->get();
-        $messages = Message::where('reply','false')->where('receiver_id',auth()->user()->id)->get();
-        return $messages; 
+        if (Auth::check()) {
+            $messages = Message::where('reply','false')->where('receiver_id',Auth::user()->id)->get();
+            return $messages;
+        }else{
+            $messages = new \Illuminate\Database\Eloquent\Collection;
+        }
+         
     }
 
     //check if there are new notifications
     function newNotificationCount()
     {
-        $count = Notification::where('reply','false')->where('user_id',auth()->user()->id)->get()->count();
-        return $count; 
+        if (Auth::check()) {
+            $count = Notification::where('reply','false')->where('user_id',Auth::user()->id)->get()->count();
+            return $count;
+        }else{
+            return 0;
+        } 
     }
 
     function newNotifications()
     {
-        $messages = Notification::where('reply','false')->where('user_id',auth()->user()->id)->get();
-        return $messages; 
+        if (Auth::check()) {
+            $notifications = Notification::where('reply','false')->where('user_id',Auth::user()->id)->get();
+            return $notifications;
+        }else{
+            $notifications = new \Illuminate\Database\Eloquent\Collection;
+        }
+         
     }
 }
