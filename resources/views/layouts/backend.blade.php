606dd1c5219054005fc9555622c537af05ac29ba
Nnamdi Ibe <ibennamdik@gmail.com>
2022-11-24 10:38:45 +0100

    inittial commit from Endee

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -105,6 +105,7 @@
                             </div>
                         </div>
                     </div>
+
                     <div class="btn-group">
                         <div class="notifications messages no-bg">
                             <a class="btn btn-default btn-sm" data-toggle="dropdown" id="notifications_section"> <i
@@ -128,141 +129,7 @@
                                         </div>
                                     </div>
                                 @endforeach
-                                    <!-- <div class="data">
-                                        <div class="row">
-                                            <div class="col-2">
-                                                <img src="{{ asset('admin/img/mailbox_imgs/1.jpg') }}" class="message-img avatar rounded-circle"
-                                                     alt="avatar1"></div>
-                                            <div class="col-10 message-data">
-                                                <i class="fa fa-clock-o"></i>
-                                                <strong>Remo</strong>
-                                                sent you an image
-                                                <br>
-                                                <small class="primary_txt">just now.</small>
-                                                <br>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="data">
-                                        <div class="row">
-                                            <div class="col-2">
-                                                <img src="{{ asset('admin/img/mailbox_imgs/2.jpg') }}" class="message-img avatar rounded-circle"
-                                                     alt="avatar1"></div>
-                                            <div class="col-10 message-data">
-                                                <i class="fa fa-clock-o"></i>
-                                                <strong>clay</strong>
-                                                business propasals
-                                                <br>
-                                                <small class="primary_txt">20min Back.</small>
-                                                <br>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="data">
-                                        <div class="row">
-                                            <div class="col-2">
-                                                <img src="{{ asset('admin/img/mailbox_imgs/3.jpg') }}" class="message-img avatar rounded-circle"
-                                                     alt="avatar1"></div>
-                                            <div class="col-10 message-data">
-                                                <i class="fa fa-clock-o"></i>
-                                                <strong>John</strong>
-                                                meeting at Ritz
-                                                <br>
-                                                <small class="primary_txt">2hrs Back.</small>
-                                                <br>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="data">
-                                        <div class="row">
-                                            <div class="col-2">
-                                                <img src="{{ asset('admin/img/mailbox_imgs/6.jpg') }}" class="message-img avatar rounded-circle"
-                                                     alt="avatar1"></div>
-                                            <div class="col-10 message-data">
-                                                <i class="fa fa-clock-o"></i>
-                                                <strong>Luicy</strong>
-                                                Request Invitation
-                                                <br>
-                                                <small class="primary_txt">Yesterday.</small>
-                                                <br>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="data">
-                                        <div class="row">
-                                            <div class="col-2">
-                                                <img src="{{ asset('admin/img/mailbox_imgs/1.jpg') }}" class="message-img avatar rounded-circle"
-                                                     alt="avatar1"></div>
-                                            <div class="col-10 message-data">
-                                                <i class="fa fa-clock-o"></i>
-                                                <strong>Remo</strong>
-                                                sent you an image
-                                                <br>
-                                                <small class="primary_txt">just now.</small>
-                                                <br>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="data">
-                                        <div class="row">
-                                            <div class="col-2">
-                                                <img src="{{ asset('admin/img/mailbox_imgs/2.jpg') }}" class="message-img avatar rounded-circle"
-                                                     alt="avatar1"></div>
-                                            <div class="col-10 message-data">
-                                                <i class="fa fa-clock-o"></i>
-                                                <strong>clay</strong>
-                                                business propasals
-                                                <br>
-                                                <small class="primary_txt">20min Back.</small>
-                                                <br>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="data">
-                                        <div class="row">
-                                            <div class="col-2">
-                                                <img src="{{ asset('admin/img/mailbox_imgs/3.jpg') }}" class="message-img avatar rounded-circle"
-                                                     alt="avatar1"></div>
-                                            <div class="col-10 message-data">
-                                                <i class="fa fa-clock-o"></i>
-                                                <strong>John</strong>
-                                                meeting at Ritz
-                                                <br>
-                                                <small class="primary_txt">2hrs Back.</small>
-                                                <br>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="data">
-                                        <div class="row">
-                                            <div class="col-2">
-                                                <img src="{{ asset('admin/img/mailbox_imgs/6.jpg') }}" class="message-img avatar rounded-circle"
-                                                     alt="avatar1"></div>
-                                            <div class="col-10 message-data">
-                                                <i class="fa fa-clock-o"></i>
-                                                <strong>Luicy</strong>
-                                                Request Invitation
-                                                <br>
-                                                <small class="primary_txt">Yesterday.</small>
-                                                <br>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="data">
-                                        <div class="row">
-                                            <div class="col-2">
-                                                <img src="{{ asset('admin/img/mailbox_imgs/1.jpg') }}" class="message-img avatar rounded-circle"
-                                                     alt="avatar1"></div>
-                                            <div class="col-10 message-data">
-                                                <i class="fa fa-clock-o"></i>
-                                                <strong>Remo</strong>
-                                                sent you an image
-                                                <br>
-                                                <small class="primary_txt">just now.</small>
-                                                <br>
-                                            </div>
-                                        </div>
-                                    </div> -->
+                                   
                                 </div>
                                 <div class="popover-footer">
                                     <a href="#" class="text-white">View All</a>
@@ -278,6 +145,7 @@
                             </a>
                         </div>
                     </div> -->
+                    
                     <div class="btn-group">
                         <div class="user-settings no-bg">
                             <button type="button" class="btn btn-default no-bg micheal_btn" data-toggle="dropdown">
